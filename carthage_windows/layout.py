8458ed8e8e83269d848cb3114bd86cb44ebc6aa9
Sam Hartman <hartmans@debian.org>
2024-09-10 14:04:50 -0600

    Initial commit

---
+++ carthage_windows/layout.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2024, Hadron Industries.
+# Carthage is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation. It is distributed
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
+# LICENSE for details.
+
+
+from carthage import *
+from carthage.modeling import *
+from carthage.ansible import *
+from carthage.network import V4Config
